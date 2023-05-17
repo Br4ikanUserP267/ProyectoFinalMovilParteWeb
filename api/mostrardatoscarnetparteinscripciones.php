9d32a8e587aa5baa6baa673a3ae765b912c87733
Braikan123 <braikanpisa2604@gmail.com>
2023-05-17 14:08:35 -0500

    Ya se puede consumir todos los datos de la api :)

---
+++ api/mostrardatoscarnetparteinscripciones.php
@@ -0,0 +1,27 @@
+<?php
+    include "../components/conexion.php";
+    include "../settings/configuraciones.php";
+
+    $dbConn = connect($db);
+
+    if (isset($_GET['numeroIdentificacion'])) {
+        $numeroIdentificacion = $_GET['numeroIdentificacion'];
+    
+        $sql = "SELECT i.*
+                FROM inscripciones i
+                INNER JOIN estudiantes e ON e.id = i.estudiantes_id
+                WHERE e.numeroIdentificacion = :numeroIdentificacion";
+        
+        $statement = $dbConn->prepare($sql);
+        $statement->bindValue(':numeroIdentificacion', $numeroIdentificacion);
+        $statement->execute();
+    
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        header("HTTP/1.1 200 OK");
+        echo json_encode($result);
+        exit();
+    }
+
+
+    //Ruta http://localhost/proyectoFinal/api/mostrardatoscarnetparteinscripciones.php?numeroIdentificacion=1103739024
+?>
